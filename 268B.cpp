1d8941d61a82c58d7a610efba646c05ea829839a
Akansha2202 <mini.akansha007@gmail.com>
2019-10-22 16:41:26 +0530

    Added solution to 268B

---
+++ 268B.cpp
@@ -0,0 +1,16 @@
+/*
+ * Author: Akansha(akansha_2202)
+ * Date : 22/10/2019
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+	long long n;
+       cin>>n;
+       cout<<(n*n*n-n)/6+n;
+	cout<<endl;
+ return 0;
+} 
